6d7c1d1605072d9643a21c2bc263e1b5fb0c51a4
Melissa Zhang <melissazhangyz@outlook.com>
2024-01-22 19:58:03 -0500

    change in downloading script

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -10,13 +10,13 @@
 library(opendatatoronto)
 library(tidyverse)
 
-
 #### Download data ####
-raw_data <- read.csv("~/Documents/UofT/3rd year/STA302/sta302paper1/inputs/data")
+raw_crisis_data <- 
+  read_csv("https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/persons-in-crisis-calls-for-service-attended/resource/4c8c589f-59d6-480e-81eb-7850dbdc1717/download/Persons%20in%20Crisis%20-%20Calls%20for%20Service%20Attended.csv")
 
 #### Save data ####
-# [...UPDATE THIS...]
+
 # change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(raw_crisis_data, "inputs/data/raw_data.csv") 
 
          
