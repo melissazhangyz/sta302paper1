db02900b89a663d4f8572a35f4a2915b0070a91d
Melissa Zhang <melissazhangyz@outlook.com>
2024-01-23 02:48:15 -0500

    test data script

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,15 +1,34 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Test for data set
+# Author: Yingzhi Zhang
+# Date: 22 January 2024
+# Contact: yingzhi.zhang@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+read_csv("outputs/data/cleaned_data.csv")
 
-#### Test data ####
+#### Test types are correct####
+types_of_crisis <- c("Person in Crisis", "Overdose", "Suicide-related")
+sort(unique(cleaned_data$Type)) == sort(types_of_crisis)
+
+#### Test year data####
+min(cleaned_data$Year) == 2014
+max(cleaned_data$Year) == 2023
+is.integer(cleaned_data$Year) == TRUE
+
+#### Test month data####
+is.character(cleaned_data$Month) == TRUE
+sort(unique(cleaned_data$Month)) == c("01", "02", "03", "04", "05", "06", "07",
+                                     "08", "09", "10", "11", "12")
+
+#### Test occurrence and apprehension data####
+sort(unique(cleaned_data$Occurence)) == sort(c("Yes", "No"))
+sort(unique(cleaned_data$Apprehension)) == sort(c("Yes", "No"))
+
+#### Test neighborhood number####
+min(cleaned_data$Neighbourhood_number, na.rm = TRUE) >= 1
+max(cleaned_data$Neighbourhood_number, na.rm = TRUE) <= 180
+is.integer(cleaned_data$Neighbourhood_number) == TRUE
